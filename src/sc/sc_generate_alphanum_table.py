eed55dbe1fcb1a632bdd732308b934776ed8bbfb
colebob9 <colebob9@gmail.com>
2021-03-16 01:19:00 -0500

    One more comment

--- src/sc/sc_generate_alphanum_table.py
+++ src/sc/sc_generate_alphanum_table.py
@@ -6,7 +6,7 @@ def alphanumTable():
     lines = open("alphanum.tbl", 'r', encoding="shift-jis").readlines()
 
     # Numbers
-    numcounter = 0x0092  # 146
+    numcounter = 0x0092
     for x in range(0, 10):
         line = lines[x]
         num = int(line[0:4], 16)
@@ -14,7 +14,7 @@ def alphanumTable():
         numcounter += 1
 
     # Capital letters
-    capcounter = 0x009c  # 156
+    capcounter = 0x009c
     for x in range(9, 35):
         line = lines[x]
         cap = int(line[0:4], 16)
@@ -22,7 +22,7 @@ def alphanumTable():
         capcounter += 1
 
     # Lowercase letters
-    lowcounter = 0x00b6  # 182
+    lowcounter = 0x00b6
     for x in range(35, 61):
         line = lines[x]
         low = int(line[0:4], 16)
